cba0d1be6cbb6ec833fb4281cd3ea4a83e692fe2
didianca <cazaranka@gmail.com>
2019-03-21 14:44:49 -0400

    testing 1

--- routes/genres.js
+++ routes/genres.js
@@ -1,18 +1,19 @@
+const validateObjectId = require('../middleware/validateObjectId');
 const express = require('express');
 const router = express.Router();
 const {Genre, validate} = require('../models/genre');
 const auth = require('../middleware/auth');
 const admin = require('../middleware/admin');
+const mongoose = require('mongoose');
 //TODO: put auth && admin after testing
 
 //get api/genres
 router.get('/', async (req, res) => {
-    //test: throw new Error('Could not get the genres.');
     const genres = await Genre.find().sort('name');
     res.send(genres);
 });
 //POST /api/genres
-router.post('/' , async (req, res) => {
+router.post('/', auth,async (req, res) => {
     const {error} = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
@@ -36,7 +37,7 @@ router.put('/:id', async (req, res) => {
     res.send(genre);
 });
 //DELETE /api/genres/:id --> delete existing genre by id
-router.delete('/:id',async (req, res) => {   //check with solution
+router.delete('/:id', async (req, res) => {
     const genre = await Genre.findByIdAndDelete(req.params.id);
     if (!genre) return res.status(404).send('The genre with the given ID was not found.');
 
@@ -44,7 +45,7 @@ router.delete('/:id',async (req, res) => {   //check with solution
     res.send(genre);
 });
 // get api/genre/:id --> (get a specific genre by id)
-router.get('/:id', async (req, res) => {   //check with solution
+router.get('/:id', validateObjectId,async (req, res) => {
     const genre = await Genre.findById(req.params.id);
     if (!genre) return res.status(404).send('The genre with the given ID was not found');
     res.send(genre);
