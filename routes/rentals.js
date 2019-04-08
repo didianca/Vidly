15819528f9e9a3c3551fe3a5a23bbcbefbfd29a4
didianca <cazaranka@gmail.com>
2019-04-08 11:19:35 -0400

    fixed some bug

--- routes/rentals.js
+++ routes/rentals.js
@@ -61,18 +61,31 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   const {error} = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
-  
-  const rental = await Rental.findByIdAndUpdate(req.params.id, {
-    name: req.body.name,
-    phone: req.body.phone,
-    isGold: req.body.isGold
+
+  const customer = await Customer.findOne({_id:req.body.customerId});
+  if (!customer) return res.status(400).send('Invalid customer...');
+
+  const movie = await Movie.findById(req.body.movieId);
+  if (!movie) return res.status(400).send('Invalid movie');
+
+  const rental = await Rental.findOneAndUpdate({_id:req.params.id}, {
+    customer: {
+      _id: customer._id,
+      name: customer.name,
+      phone: customer.phone
+    },
+    movie: {
+      _id: movie._id,
+      title: movie.title,
+      dailyRentalRate: movie.dailyRentalRate
+    }
   }, {new: true});
   if (!rental) return res.status(404).send('The rental object with the given ID was not found.');
   
   res.send(rental);
 });
 //GET api/rentals/:id
-router.get('/:id', async (req, res) => {   //check with solution
+router.get('/:id', async (req, res) => {
   const rental = await Rental.findById(req.params.id);
   if (!rental) return res.status(404).send('The rental object with the given ID was not found');
   res.send(rental);
